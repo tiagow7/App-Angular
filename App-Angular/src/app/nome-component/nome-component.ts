564bf27c5959a6bf08453bfec097a359a66e3e42
TIAGO DA SILVA SANTOS <tiagow2013@gmail.com>
2025-10-31 16:44:54 -0300

    criando component

---
+++ App-Angular/src/app/nome-component/nome-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nome-component',
+  imports: [],
+  templateUrl: './nome-component.html',
+  styleUrl: './nome-component.scss',
+})
+export class NomeComponent {
+
+}
